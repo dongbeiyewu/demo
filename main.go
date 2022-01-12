aca71c1ebfa207faf64c8d673a1c8a7348e8a0f5
dongbeiyewu <2698922550@qq.com>
2022-01-12 16:48:52 +0800

    Standardized configuration

--- main.go
+++ main.go
@@ -2,18 +2,39 @@ package main
 
 import (
 	"demo/common"
+	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-
+	//log.Println("读取错误")
+	InitConfig()
+	//log.Println("读取错误")
 	db := common.InitDB()
 	defer db.Close()
 
 	r := gin.Default()
 	r = CollectRoute(r)
+	port := viper.GetString("server.port")
+	if port != "" {
+		panic(r.Run(":" + port))
+	}
 	panic(r.Run()) // listen and serve on 0.0.0.0:8080
+}
+
+func InitConfig() {
+	//log.Println("读取错误")
+	workDir, _ := os.Getwd()
+	viper.SetConfigName("application")
+	viper.SetConfigType("yml")
+	viper.AddConfigPath(workDir + "/config")
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Println("读取错误", err)
+	}
 }
